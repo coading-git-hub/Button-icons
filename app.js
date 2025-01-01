0faaa5d13d409e12b6f2dc73cdfadd2dea46e8a2
KIRAN AHMED <162012113+coading-git-hub@users.noreply.github.com>
2025-01-02 00:23:56 +0500

    work done

---
+++ app.js
@@ -0,0 +1,24 @@
+// 1 button
+
+
+
+  // button 3
+
+  // $(function () {
+  //   $("#btn").click(function () {
+  //     $("#btn").addClass("onclic", 250, validate);
+  //   });
+  
+  //   function validate() {
+  //     setTimeout(function () {
+  //       $("#btn").removeClass("onclic");
+  //       $("#btn").addClass("validate", 450, callback);
+  //     }, 2250);
+  //   }
+  //   function callback() {
+  //     setTimeout(function () {
+  //       $("#btn").removeClass("validate");
+  //     }, 1250);
+  //   }
+  // });
+  
